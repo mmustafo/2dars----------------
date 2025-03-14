c30e82f0ceea019bc8121847d9654e35a02257ff
mustafo <mustafo@gmail.com>
2025-03-14 10:12:48 +0500

    in coment

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -15,7 +15,7 @@ function Modal({ setShowModal, t, deleteTitle }) {
             className="px-5 py-2 bg-gray-300 text-gray-700 rounded-lg hover:bg-gray-400 transition"
             onClick={() => setShowModal(false)}
           >
-            oyladm buni ochmayman
+            oyladm buni ochrmayman
           </button>
           <button
             className="px-5 py-2 bg-red-500 text-white rounded-lg hover:bg-red-600 transition"
